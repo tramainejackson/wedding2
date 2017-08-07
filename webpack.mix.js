454d68440f6991da43b9d9ef4edf1493f1023e0f
Tramaine Jackson <jackson.tramaine3@gmail.com>
2017-08-06 20:44:00 -0400

    Update v.2

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', '/js')
-   .js('node_modules/jquery/dist/jquery.min.js', '/js')
-   .js('node_modules/materialize-css/dist/js/materialize.min.js', '/js')
-   .sass('resources/assets/sass/app.scss', '/css');
+mix.js('resources/assets/js/app.js', 'public/js')
+   .js('node_modules/jquery/dist/jquery.min.js', 'public/js')
+   .js('node_modules/materialize-css/dist/js/materialize.min.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
