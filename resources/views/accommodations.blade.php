e23e96303773decd8f7fd6a7806208f8491cfeeb
Tramaine Jackson <jackson.tramaine3@gmail.com>
2017-08-14 12:53:43 -0400

    Updated gold bgrd v.2.3 Updated mobile for accomodations

--- resources/views/accommodations.blade.php
+++ resources/views/accommodations.blade.php
@@ -20,7 +20,7 @@
 	</header> --->
 	<header class="w3-display-container w3-wide bgimg w3-grayscale-min" id="home">
 		<div class="w3-display-middle w3-text-white w3-center headerContent">
-			<h1 class="w3-jumbo">Guest Accommodations</h1>
+			<h1 class="w3-jumbo guestAccomHeader">Guest Accommodations</h1>
 			<span class="w3-text-white w3-display-bottommiddle w3-hide-small"><i></i></span>
 		</div>
 	</header>
@@ -38,27 +38,27 @@
 			<h1 class="w3-center w3-jumbo" style="border-bottom: solid 1px; margin: 0% 20% 5%; padding: 2% 0%;">Hotels</h1>
 		</div>
 		<div class="row">	
-			<div class="col s4 m4 l4 w3-padding">
+			<div class="col s12 m4 l4 w3-padding">
 				<h2 class="w3-center">Fairfield Inn & Suites by Marriott Mt. Laurel</h2>
 				<h3 class="w3-center"><i>(856) 642-0600</i></h4>
 				<h4 class="w3-center"><a href="www.fairfieldinn.com/phlfm" class="">www.fairfieldinn.com/phlfm</a></h4>
 			</div>
-			<div class="col s4 m4 l4 w3-padding">
+			<div class="col s12 m4 l4 w3-padding">
 				<h2 class="w3-center">Hampton Inn Cherry Hill/Voorhees</h2>
 				<h3 class="w3-center"><i>(856) 346-4500</i></h3>
 				<h4 class="w3-center"><a href="www.hamptonvoorhees.com" class="">www.hamptonvoorhees.com</a></h4>
 			</div>
-			<div class="col s4 m4 l4 w3-padding">
+			<div class="col s12 m4 l4 w3-padding">
 				<h2 class="w3-center">SpringHill Suites by Marriott Voorhees Mt. Laurel Cherry Hill</h2>
 				<h3 class="w3-center"><i>(856) 782-2555</i></h3>
 				<h4 class="w3-center"><a href="www.marriott.com/PHLVO" class="">www.marriott.com/PHLVO</a></h4>
 			</div>
-			<div class="col offset-s6 offset-m4 m4 offset-l2 l4 w3-padding">
+			<div class="col s12 offset-m4 m4 offset-l2 l4 w3-padding">
 				<h2 class="w3-center">Staybridge Suites Philadelphia – Mt. Laurel</h2>
 				<h3 class="w3-center"><i>(856) 722-1900</i></h3>
 				<h4 class="w3-center"><a href="www.staybridge.com/mtlaurel" class="">www.staybridge.com/mtlaurel</a></h4>
 			</div>
-			<div class="col s6 offset-m4 m4 l4 w3-padding">
+			<div class="col s12 offset-m4 m4 l4 w3-padding">
 				<h2 class="w3-center w3-padding">Wingate by Wyndham Hotel</h2>
 				<h3 class="w3-center"><i>(856) 627-1000</i></h3>
 				<h4 class="w3-center"><a href="www.wingatehotels.com" class="">www.wingatehotels.com</a></h4>
@@ -70,8 +70,8 @@
 @section('wedding_information')
 	<!-- Directions -->
 	<div class="w3-container w3-padding-64 w3-pale-red w3-grayscale-min" id="us" style="background: linear-gradient(#f5f8fa, #ffdddd, #ffdddd, #ffdddd);">
-		<div class="w3-content" style="border-bottom: solid 1px; margin: 0% 20% 5%; padding: 2% 0%;">
-			<h1 class="w3-center w3-jumbo">Directions</h1>
+		<div class="w3-content directionsDiv" style="border-bottom: solid 1px; margin: 0% 20% 5%; padding: 2% 0%;">
+			<h1 class="w3-center w3-jumbo" style="max-width:100%;">Directions</h1>
 			<h3 class="w3-center">81 West White Horse Pike, Berlin, NJ 08009</h1>
 		</div>
 		<div class="row">	
